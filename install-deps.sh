03a811d94aa043cb6e2f1d00e8779cccc64362b0
Cameron Smith <camerons03@vt.edu>
2025-11-15 17:37:59 -0500

    cleans up local files now from install script

--- install-deps.sh
+++ install-deps.sh
@@ -33,6 +33,8 @@ if [[ "$ID" == "debian" ]] || [[ "$ID" == "ubuntu" ]]; then
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit -D -t /usr/local/bin/
+    rm -rf lazygit.tar.gz 
+    rm -rf lazygit
   else
     DEPS="$DEPS lazygit tree-sitter-cli"
   fi
