a9c304166acbab7d02ae30b07b669bec2ddb4be9
Cameron Smith <camerons03@vt.edu>
2025-11-15 15:33:56 -0500

    added nvim-spectre for global search and replace

--- lua/cameron/core/keymaps.lua
+++ lua/cameron/core/keymaps.lua
@@ -2,28 +2,72 @@ vim.g.mapleader = ","
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "jk", "<ESC>", {
+	desc = "Exit insert mode with jk",
+})
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights " })
+keymap.set("n", "<leader>nh", ":nohl<CR>", {
+	desc = "Clear search highlights",
+})
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-keymap.set("n", "<leader>sk", "<C-w>>", { desc = "Make split window larger" })
-keymap.set("n", "<leader>sj", "<C-w><", { desc = "Make split window smaller" })
+keymap.set("n", "<leader>sv", "<C-w>v", {
+	desc = "Split window vertically",
+})
+keymap.set("n", "<leader>sh", "<C-w>s", {
+	desc = "Split window horizontally",
+})
+keymap.set("n", "<leader>se", "<C-w>=", {
+	desc = "Make splits equal size",
+})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {
+	desc = "Close current split",
+})
+keymap.set("n", "<leader>sk", "<C-w>>", {
+	desc = "Make split window larger",
+})
+keymap.set("n", "<leader>sj", "<C-w><", {
+	desc = "Make split window smaller",
+})
+
 -- To switch between windows:
 --   <C-[hjkl]>
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {
+	desc = "Open new tab",
+})
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {
+	desc = "Close current tab",
+})
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {
+	desc = "Go to next tab",
+})
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {
+	desc = "Go to previous tab",
+})
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {
+	desc = "Open current buffer in new tab",
+})
 
 -- Switch out of TERMINAL mode to swap windows
-keymap.set("t", "<C-t>", "<C-\\><C-n>:echo<CR>", { noremap = true, silent = true })
+keymap.set("t", "<C-t>", "<C-\\><C-n>:echo<CR>", {
+	noremap = true,
+	silent = true,
+})
+
+-- Nvim-Spectre
+keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+	desc = "Toggle Spectre",
+})
+keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+	desc = "Search current word",
+})
+keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+	desc = "Search current word",
+})
+keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+	desc = "Search on current file",
+})
 
 -- Disable arrow keys to prevent myself from abusing them
 -- keymap.set("n", "<Up>", "<Nop>", { noremap = true })
