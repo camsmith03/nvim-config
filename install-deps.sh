b15b0e1eeab69361b9f4d6e26e2bb40710efbdfa
Cameron Smith <camerons03@vt.edu>
2025-11-15 16:54:31 -0500

    added extra deps

--- install-deps.sh
+++ install-deps.sh
@@ -23,19 +23,28 @@ PKGMGR=""
 PKGMGR_OPTS=""
 DEPS=""
 
-if [[ "$ID" == "debian" ]]; then
+if [[ "$ID" == "debian" ]] || [[ "$ID" == "ubuntu" ]]; then
 	PKGMGR="apt"
 	PKGMGR_OPTS="install -y"
-	DEPS="gcc build-essential git curl wget unzip ripgrep fd-find xclip luarocks golang cargo ruby-full php composer default-jdk perl cpanminus tree-sitter-cli pandoc python3-full"
+	DEPS="gcc build-essential git curl wget unzip ripgrep fd-find xclip luarocks golang cargo ruby-full php composer default-jdk perl cpanminus tree-sitter-cli pandoc python3-full clangd"
 	sudo $PKGMGR update
+  if ! [[ "$VERSION_ID" == "13" ]]; then 
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit -D -t /usr/local/bin/
+  else
+    DEPS="$DEPS lazygit"
+  fi
 elif [[ "$ID" == "fedora" ]]; then
 	PKGMGR="dnf"
 	PKGMGR_OPTS="install -y"
-	DEPS="gcc gcc-c++ make git curl wget unzip ripgrep fd-find xclip python3 luarocks golang cargo ruby ruby-devel php composer java-latest-openjdk-devel perl perl-App-cpanminus tree-sitter-cli pandoc"
+	DEPS="gcc gcc-c++ make git curl wget unzip ripgrep fd-find xclip python3 luarocks golang cargo ruby ruby-devel php composer perl perl-App-cpanminus tree-sitter-cli pandoc clangd lazygit"
+  sudo $PKGMGR copr enable dejan/lazygit
 elif [[ "$ID" == "arch" ]]; then
 	PKGMGR="pacman"
 	PKGMGR_OPTS="-S --noconfirm --needed"
-	DEPS="base-devel git curl wget unzip ripgrep fd xclip python luarocks go rust ruby php composer jdk-openjdk perl perl-app-cpanminus tree-sitter-cli pandoc"
+	DEPS="base-devel git curl wget unzip ripgrep fd xclip python luarocks go rust ruby php composer jdk-openjdk perl perl-app-cpanminus tree-sitter-cli pandoc clangd lazygit"
 	sudo $PKGMGR -Syu
 else 
 	echo "Unsupported distribution: $ID"
@@ -47,12 +56,22 @@ echo "Dependencies: $DEPS"
 sudo $PKGMGR $PKGMGR_OPTS $DEPS
 
 if ! command -v npm &> /dev/null; then
-	echo "Installing npm..."
+  echo "Installing npm (using nvm)..."
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 	export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 	nvm install 24
+fi
+
+if ! command -v java &> /dev/null && ! command -v javac &> /dev/null; then
+  if ! command -v sdk &> /dev/null; then 
+    echo "Intalling sdkman..."
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+  fi
+  echo "Installing java..."
+  sdk install java
 fi
 
 if ! command -v julia &> /dev/null; then
@@ -67,7 +86,7 @@ if ! command -v uv &> /dev/null; then
 fi
 
 if command -v gem &> /dev/null; then
-	echo "Installing neovim gem package..."
+	echo "Installing gem package for neovim..."
 	gem install neovim
 fi
 
